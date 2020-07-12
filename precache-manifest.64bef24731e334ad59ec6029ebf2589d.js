79f6d98cc781a018e2843ea0eb1032c7c5d9a852
aaroncaraway <aaroncaraway42@gmail.com>
2020-07-12 15:17:05 -0700

    Updates

---
+++ precache-manifest.64bef24731e334ad59ec6029ebf2589d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d01eb13679ce2c672e99eeac5e7e63f3",
+    "url": "/umre-todo/index.html"
+  },
+  {
+    "revision": "f00027d0f9a1732faac3",
+    "url": "/umre-todo/static/js/2.94afdf39.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/umre-todo/static/js/2.94afdf39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76b9e0f1d16e886110f8",
+    "url": "/umre-todo/static/js/main.b00e6002.chunk.js"
+  },
+  {
+    "revision": "56273545f87c3788d713",
+    "url": "/umre-todo/static/js/runtime-main.7de9a1df.js"
+  }
+]);
